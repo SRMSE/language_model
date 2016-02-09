db646b7bc498933189eee5d279e9c66256cdfb5f
akhil <akhilgupta.official@gmail.com>
2016-02-09 21:37:14 +0530

    added route for both spellcheck and relation

--- api.py
+++ api.py
@@ -1,6 +1,11 @@
 from bottle import route,run,response
 from middleware import autoMain
+from load_data import NWORDS
+from spellcheck import correct
+import retrievalmain as rm
+import probability as p
 import tags
+
 @route('/')
 def hello():
 	return "Welcome to the language model"
@@ -8,6 +13,22 @@ def hello():
 @route('/language_model/')
 def default_list():
     return "Welcome to the language model"
+
+@route('/language_model/start')
+def load_data():
+    return NWORDS()
+
+
+@route('/language_model/relation/<first_word>/<second_word>/',method = 'GET')
+def relFirstToSecond(first_word = " ",second_word = " "):
+	probable_words= rm.rel_given_to_prev(second_word,first_word)
+	return {"realtion" : probable_words}
+
+
+
+@route('/language_model/spellcheck/<name>',method = 'GET')
+def spellcheck(name = " " ):
+    return {"candidates":correct(name)}
 
 @route('/get_tags/<name>/<top_n>',method = 'GET')
 def get_tags(name = " ",top_n = " "):
